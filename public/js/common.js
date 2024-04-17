c41f598229f6ab8cc844d9bbae29858a63bada99
youzhen <yl301114@gmail.com>
2024-04-17 16:10:07 +0100

    首頁

---
+++ public/js/common.js
@@ -0,0 +1,16 @@
+$(".header_menu").click(function(){
+    let scroll = $(window).scrollTop();
+    if(scroll <= 0) {
+        $(this).parents(".header").toggleClass("active");
+    }
+    $(this).toggleClass("active");
+    $(".header_nav").toggleClass("active");
+})
+
+$(window).on("scroll resize", function(){
+    if($(window).scrollTop()>0){
+        $(".header").addClass("active");
+    }else {
+        $(".header").removeClass("active");
+    }
+})
